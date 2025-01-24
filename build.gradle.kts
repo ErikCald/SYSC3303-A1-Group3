7db482c85701499f22e8370c9bb96972c200e06b
Konicai <71294714+Konicai@users.noreply.github.com>
2025-01-24 14:35:59 -0500

    Setup gradle project (#1)

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+
+plugins {
+    // Apply the application plugin to add support for an application in Java.
+    application
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // Use JUnit Jupiter for testing.
+    testImplementation(libs.junit.jupiter)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass = "sysc3303.a1.group3"
+}
+
+tasks.test {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
+}
