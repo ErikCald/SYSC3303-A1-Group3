0d884876d5198d7ce9df92028c88a25dfc6d4ecb
Konicai <71294714+Konicai@users.noreply.github.com>
2025-01-31 17:29:03 -0500

    Formatting and exception handling

--- src/main/java/sysc3303/a1/group3/Parser.java
+++ src/main/java/sysc3303/a1/group3/Parser.java
@@ -27,8 +27,8 @@ public class Parser {
      *  
      * @param file The file to parse.
      */
-    public ArrayList<Event> parseIncidientFile(InputStream file) {
-        ArrayList<Event> events = new ArrayList<>();
+    public List<Event> parseIncidentFile(InputStream file) throws IOException {
+        List<Event> events = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(file))) {
             String line;
@@ -36,12 +36,9 @@ public class Parser {
                 try {
                     events.add(parseIncidentLine(line));
                 } catch (IllegalArgumentException e) {
-                    System.err.printf("Error parsing line: %s with exception: %s, continuing to next line.%n", line, e);
-                    continue;
+                    throw new IOException("Failed to parse line: " + line, e);
                 }
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Error reading incident file with exception: " + e);
         }
 
         return events;
@@ -57,26 +54,20 @@ public class Parser {
     private Event parseIncidentLine(String line) throws IllegalArgumentException {
         String[] values = line.split(",");
 
-        if(values.length < 4) {
-            throw new IllegalArgumentException("Parse Error: Not enough coloums in line: " + line);
+        if (values.length < 4) {
+            throw new IllegalArgumentException("Not enough columns in line: " + line);
         }
 
-        try {
-            Time time = Time.valueOf(values[0]);
-            int zoneId = Integer.parseInt(values[1]);
-            EventType eventType = EventType.fromString(values[2]);
-            Severity severity = Severity.fromString(values[3]);
+        Time time = Time.valueOf(values[0]);
+        int zoneId = Integer.parseInt(values[1]);
+        EventType eventType = EventType.fromString(values[2]);
+        Severity severity = Severity.fromString(values[3]);
 
-            return new Event(time, zoneId, eventType, severity);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("Invalid line: " + line);
-        }
+        return new Event(time, zoneId, eventType, severity);
     }
 
-
-
-    public List<Zone> parseZoneFile(InputStream file) {
-        List<Zone> zones = new ArrayList<Zone>() {};
+    public List<Zone> parseZoneFile(InputStream file) throws IOException {
+        List<Zone> zones = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(file))) {
             String line;
@@ -84,12 +75,9 @@ public class Parser {
                 try {
                     zones.add(parseZoneFileLine(line));
                 } catch (IllegalArgumentException e) {
-                    System.err.printf("Error parsing line: %s with exception: %s, Stopping process", line, e);
-                    break;
+                    throw new IOException("Failed to parse line: " + line, e);
                 }
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Error reading Zone file with exception: " + e);
         }
 
         return zones;
@@ -106,23 +94,15 @@ public class Parser {
         String[] values = line.split(",");
 
         if (values.length < 4) {
-            throw new IllegalArgumentException("Parse Error: Not enough coloums in line: " + line);
+            throw new IllegalArgumentException("Not enough columns in line: " + line);
         }
 
-        try {
+        int zoneId = Integer.parseInt(values[0]);
+        int startx = Integer.parseInt(values[1]);
+        int starty = Integer.parseInt(values[2]);
+        int endx = Integer.parseInt(values[3]);
+        int endy = Integer.parseInt(values[4]);
 
-            int zoneId = Integer.parseInt(values[0]);
-            int startx = Integer.parseInt(values[1]);
-            int starty = Integer.parseInt(values[2]);
-            int endx = Integer.parseInt(values[3]);
-            int endy = Integer.parseInt(values[4]);
-
-            return new Zone(zoneId, startx, starty, endx, endy);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("Can not create Zone object using provided parameters " + line);
-        }
+        return new Zone(zoneId, startx, starty, endx, endy);
     }
-
-
-
 }
