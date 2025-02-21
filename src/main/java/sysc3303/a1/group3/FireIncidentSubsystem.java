b67150ae32ea5a6b4670e8237a66aa48f1429b21
Lakshman Chelliah <lakshman.chelliah@gmail.com>
2025-02-21 01:47:06 -0500

    Add Timed Events to Fire Incident Subsystem and Modify Event.getTime

--- src/main/java/sysc3303/a1/group3/FireIncidentSubsystem.java
+++ src/main/java/sysc3303/a1/group3/FireIncidentSubsystem.java
@@ -1,9 +1,13 @@
 package sysc3303.a1.group3;
 
 import java.io.IOException;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.time.Duration;
 
 
 /**
@@ -12,17 +16,18 @@ import java.util.List;
  * acknowledgments.
  */
 public class FireIncidentSubsystem implements Runnable {
-
+    volatile int elapsedSeconds;
     private final Scheduler scheduler;
     private List<Event> events;
     private List<Zone> zones;
     private int eventCount;
-
+    private int currentSeconds = 0;
+    Instant startTime;
     public FireIncidentSubsystem(Scheduler s, InputStream fileStream) throws IOException {
         this.scheduler = s;
         eventCount = 0;
         events = new ArrayList<>();
-
+        startTime = Instant.now();
         Parser parser = new Parser();
         if (fileStream == null) {
             System.out.println("Incident file doesn't exist");
@@ -35,19 +40,21 @@ public class FireIncidentSubsystem implements Runnable {
     // Start the subsystem
     @Override
     public void run() {
-        for (int i = 0; i < events.size(); i++) {
-            Event event = events.get(i);
+        for (Event event: events) {
+
+            do {
+                Instant currentTime = Instant.now();
+                elapsedSeconds = (int) Duration.between(startTime, currentTime).getSeconds();
+                //break;
+            } while (elapsedSeconds <= event.getTime());
+
             ++eventCount;
             System.out.println("Sending Event " + eventCount + " to Scheduler: \n" + event + "\n");
             scheduler.addEvent(event);
 
             // I have left it commented out in case the TAs want to slow the output down for demonstration
             // It should not mess the implementation up at this iteration, but I agree it should be removed later.
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.out.println(e);
-            }
+
         }
         System.out.println("All events have been exhausted, " + Thread.currentThread().getName() + " , is closing.");
         sendShutOffSignal();
