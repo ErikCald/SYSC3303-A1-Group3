d5336143e9d1fb4400af0a1ee2f3fb03bda7e25d
TimXia7 <timxia6208@gmail.com>
2025-04-03 14:45:04 -0400

    12 drone, 4 differently sized zone testing

--- src/main/java/sysc3303/a1/group3/Main.java
+++ src/main/java/sysc3303/a1/group3/Main.java
@@ -48,7 +48,7 @@ public class Main {
 
         // Get user input for number of drones, or just modify numDrones
         Scanner scanner = new Scanner(System.in);
-        int numDrones = 3;
+        int numDrones = 12;
 
         while (numDrones <= 0) {
             try {
