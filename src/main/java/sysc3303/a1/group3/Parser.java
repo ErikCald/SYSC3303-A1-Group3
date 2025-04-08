d627d2a616323d122bdfaa4a31aa3f2fdb342615
TimXia7 <timxia6208@gmail.com>
2025-04-08 16:07:29 -0400

    revert time scaling

--- src/main/java/sysc3303/a1/group3/Parser.java
+++ src/main/java/sysc3303/a1/group3/Parser.java
@@ -68,26 +68,11 @@ public class Parser {
         }
         Time time = Time.valueOf(values[0]);
 
-        // Extract hours, minutes, and seconds
-        int hours = time.getHours();
-        int minutes = time.getMinutes();
-        int seconds = time.getSeconds();
-
-        // Convert to seconds, and divide/adjust:
-        int totalSeconds = hours * 3600 + minutes * 60 + seconds;
-        int adjustedTotalSeconds = totalSeconds / 10;
-        int adjustedHours = adjustedTotalSeconds / 3600;
-        int adjustedMinutes = (adjustedTotalSeconds % 3600) / 60;
-        int adjustedSeconds = adjustedTotalSeconds % 60;
-
-        // New time object
-        Time adjustedTime = new Time(adjustedHours, adjustedMinutes, adjustedSeconds);
-
         int zoneId = Integer.parseInt(values[1]);
         EventType eventType = EventType.fromString(values[2]);
         Severity severity = Severity.fromString(values[3]);
 
-        return new Event(adjustedTime , zoneId, eventType, severity);
+        return new Event(time , zoneId, eventType, severity);
     }
 
     public List<Zone> parseZoneFile(InputStream file) throws IOException {
